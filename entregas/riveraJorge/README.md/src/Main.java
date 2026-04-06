b8aede77afacec19981eaba37d28ff19b8400d63
jerb05 <jerb0508@gmail.com>
2026-04-06 19:15:24 +0200

    Implementacion de scanner y confirmacion de si invitado ira a la fiesta

--- entregas/riveraJorge/README.md/src/Main.java
+++ entregas/riveraJorge/README.md/src/Main.java
@@ -6,6 +6,10 @@ public class Fiesta{
     mostrar Articulos
     mostrar CosteTotal
 
-    
+    Scanner sc.Scanner (System.in);
+    System.out.println("Asistira a la fiesta?");
+    boolean (next.invitadoAsistira)
+
+    mostrar String [listaInvitados] (Fiesta);
 
 }
