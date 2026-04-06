7179d1a5cedba1b64337bfb2857ed2ba805f201e
jerb05 <jerb0508@gmail.com>
2026-04-06 19:23:03 +0200

    Main

--- entregas/riveraJorge/README.md/src/Main.java
+++ entregas/riveraJorge/README.md/src/Main.java
@@ -1,15 +1,16 @@
 public class Fiesta{
     public static void Main(String[]args);
 
+   Scanner sc.Scanner (System.in);
+
+    
+    System.out.println("Asistira a la fiesta?");
+    boolean (next.invitadoAsistira)
+
     mostrar Fiesta
     mostrar Invitado
     mostrar Articulos
     mostrar CosteTotal
-
-    Scanner sc.Scanner (System.in);
-    System.out.println("Asistira a la fiesta?");
-    boolean (next.invitadoAsistira)
-
     mostrar String [listaInvitados] (Fiesta);
 
 }
